bae7fe398e35c36b1413f4a6b247da45e6852fe6
Slucajni lik <medicinskicentarr@gmail.com>
2023-11-09 17:15:40 +0100

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,57 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Mills from './components/Mills'
+import axios from 'axios'
 
 
 function App() {
 
+const [data,setData]=useState(null)
+const [loading,setLoading]=useState(true)
+const [error,setError]=useState(null)
+
+useEffect(
+()=>{
+
+async function effect()
+{
+
+setLoading(true)
+try{
+  const response = await axios.post('http://127.0.0.1:8000/Mills/Hello/',{name:'amin',code:7889})
+   setData(response.data)
+   setError(null)
+
+}
+catch(e)
+{
+  setError(e)
+  setData(null)
+
+}
+setLoading(false)
+
+}
+effect()
+
+},[])
+
+if(loading)
+{
+  return <h1>Loading</h1>
+}
+
+if(error)
+{
+  <p>{JSON.stringify(error)}</p>
+}
 
   return (
     <>
+    <h1>{data.message}</h1>
+    <h1>{data.code}</h1>
 <Mills/>
 
 
